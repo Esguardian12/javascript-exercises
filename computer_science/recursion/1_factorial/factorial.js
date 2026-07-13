e2564433ff2df3cee27a9257e57aebe79db9804c
Esguardian12 <esmultipro@gmail.com>
2026-07-13 18:03:37 -0400

    I modified the first problem inside the computer_science folder.

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,15 @@
-const factorial = function() {
+const factorial = function(n) {
+    // 1. Validate the input: must be a number, an integer, and non-negative
+    if (typeof n !== 'number' || !Number.isInteger(n)|| n < 0){
+        return undefined;
+    }
+    // 2. Base case: an input of 0 should return 1
+    if(n === 0) {
+        return 1;
+    }
 
+    // 3. Recursive step: n * factorial(n - 1)
+    return n * factorial(n - 1);
 };
 
 // Do not edit below this line
